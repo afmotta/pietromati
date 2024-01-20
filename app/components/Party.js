cf4da60c370e1a38efa44b403e3e8bdbfc931fc2
Alberto Francesco Motta <alberto@motta.dev>
2024-01-20 22:46:26 +0100

    fix: party picture

--- app/components/Party.js
+++ app/components/Party.js
@@ -27,29 +27,22 @@ export function Party() {
           <div className='flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents'>
             <div className='w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end'>
               <img
-                src='/ricevimento-0.jpg'
+                src='/party-0.jpg'
                 alt=''
                 className='aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover'
               />
             </div>
             <div className='contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8'>
-              {/* <div className='order-first flex w-64 flex-none justify-end self-end lg:w-auto'>
-                <img
-                  src='/ricevimento-2.jpg'
-                  alt=''
-                  className='aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover'
-                />
-              </div> */}
               <div className='flex w-96 flex-auto justify-end lg:w-auto lg:flex-none'>
                 <img
-                  src='/ricevimento-1.jpg'
+                  src='/party-1.jpg'
                   alt=''
                   className='aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover'
                 />
               </div>
               <div className='hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none'>
                 <img
-                  src='/ricevimento-3.jpg'
+                  src='/party-2.jpg'
                   alt=''
                   className='aspect-[4/3] w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover'
                 />

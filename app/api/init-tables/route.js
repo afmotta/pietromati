35d26015c79ee267fab28832b3d0d2a63e1c4b5b
Alberto Francesco Motta <alberto@motta.dev>
2024-01-29 00:53:26 +0100

    safety

--- app/api/init-tables/route.js
+++ app/api/init-tables/route.js
@@ -3,12 +3,13 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    await sql`DROP TABLE rsvp`;
-    const createRsvp =
-      await sql`CREATE TABLE rsvp (id uuid NOT NULL DEFAULT gen_random_uuid(), name varchar, count smallint, email varchar, phone varchar, is_coming boolean DEFAULT true, needs_transportation boolean DEFAULT false, notes varchar);`;
-    const addIndex =
-      await sql`ALTER TABLE rsvp ADD CONSTRAINT rsvp_pk PRIMARY KEY (id);`;
-    return NextResponse.json({ createRsvp, addIndex }, { status: 200 });
+    // await sql`DROP TABLE rsvp`;
+    // const createRsvp =
+    //   await sql`CREATE TABLE rsvp (id uuid NOT NULL DEFAULT gen_random_uuid(), name varchar, count smallint, email varchar, phone varchar, is_coming boolean DEFAULT true, needs_transportation boolean DEFAULT false, notes varchar);`;
+    // const addIndex =
+    //   await sql`ALTER TABLE rsvp ADD CONSTRAINT rsvp_pk PRIMARY KEY (id);`;
+    // return NextResponse.json({ createRsvp, addIndex }, { status: 200 });
+    return NextResponse.json({ msg: 'Currently disabled' }, { status: 200 });
   } catch (error) {
     console.error(error)
     return NextResponse.json({ error }, { status: 500 });
